3035ad5e85f0e3cda3e00a066d6d9293cda5179d
Giles Johnson <bastisawesomeltd@gmail.com>
2023-12-01 12:47:26 -0500

    Cleanup tests source code.

--- tests/CMakeFiles/Bz80Tests_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Bz80Tests_autogen/mocs_compilation.cpp"
-  "CMakeFiles/Bz80Tests_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Bz80Tests_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
