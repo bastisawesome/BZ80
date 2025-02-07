bf0d83f7c15f8f482af5c7ffca1cb60e7dae93b4
Giles Johnson <bastisawesomeltd@gmail.com>
2025-02-07 14:21:04 -0500

    Implement Git-based automatic versioning.
    Closes issue #15 of the internal issue tracker.
    
    With this change, the project can now automatically track the versioning
    from Git.

---
+++ cmake/GitVersion.cmake
@@ -0,0 +1,57 @@
+function(get_version_from_git)
+    find_package(Git QUIET)
+    if(NOT Git_FOUND)
+        message(WARNING "Git not found")
+        return()
+    endif()
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --match "v*" --always
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_TAG
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        RESULT_VARIABLE GIT_RESULT
+    )
+
+    if(NOT GIT_RESULT EQUAL 0)
+        message(WARNING "Failed to get git tag; Has Git been initialised?")
+        return()
+    endif()
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} rev-parse --short=7 HEAD
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_COMMIT_SHORT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    string(REGEX REPLACE "^v" "" CLEAN_TAG "${GIT_TAG}")
+    if(CLEAN_TAG MATCHES "^([0-9]+)\\.([0-9]+)\\.([0-9]+)(-.*)?$")
+        set(MAJOR ${CMAKE_MATCH_1})
+        set(MINOR ${CMAKE_MATCH_2})
+        set(PATCH ${CMAKE_MATCH_3})
+
+        if("${CMAKE_MATCH_4}" STREQUAL "")
+            set(DIRTY "")
+        else()
+            set(DIRTY "-${GIT_COMMIT_SHORT_HASH}")
+        endif()
+    else()
+        set(MAJOR 0)
+        set(MINOR 0)
+        set(PATCH 1)
+        set(DIRTY "-${GIT_COMMIT_SHORT_HASH}")
+    endif()
+
+    set(PROJECT_VERSION_MAJOR ${MAJOR})
+    set(PROJECT_VERSION_MAJOR ${MAJOR} PARENT_SCOPE)
+    set(PROJECT_VERSION_MINOR ${MINOR})
+    set(PROJECT_VERSION_MINOR ${MINOR} PARENT_SCOPE)
+    set(PROJECT_VERSION_PATCH ${PATCH})
+    set(PROJECT_VERSION_PATCH ${PATCH} PARENT_SCOPE)
+
+    set(FULL_VERSION "${MAJOR}.${MINOR}.${PATCH}${DIRTY}")
+    set(FULL_VERSION "${MAJOR}.${MINOR}.${PATCH}${DIRTY}" PARENT_SCOPE)
+    set(PROJECT_VERSION "${MAJOR}.${MINOR}.${PATCH}")
+    set(PROJECT_VERSION "${MAJOR}.${MINOR}.${PATCH}" PARENT_SCOPE)
+endfunction()
--- src/bz80/main.cpp
+++ src/bz80/main.cpp
@@ -1,3 +1,21 @@
+#include <QDebug>
+
+#include "version.hpp"
+
+void print_version() {
+    qInfo() << "Application Version: " << VERSION << "\n";
+    qInfo() << "Full Version: " << FULL_VERSION << "\n";
+    qInfo() << "Version Details:\n";
+    qInfo() << "Major: " << VERSION_MAJOR << "\n";
+    qInfo() << "Minor: " << VERSION_MINOR << "\n";
+    qInfo() << "Patch: " << VERSION_PATCH << "\n";
+    qInfo() << "Build Date: " << __DATE__ << "\n";
+    qInfo() << "Build Time: " << __TIME__ << "\n";
+}
+
 int main() {
+    qInfo() << "Hello!\n";
+    print_version();
+
     return 0;
 }
